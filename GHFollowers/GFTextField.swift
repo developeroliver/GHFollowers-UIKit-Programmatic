448822aea7784ba2f32c8c202d4ec4f3d9db3e52
olive <developpeurolive.ios@gmail.com>
2023-05-13 19:58:26 +0200

    Finish SearchVC add constraints to logoImageView, usernameTextField and callToActionButton

--- GHFollowers/GFTextField.swift
+++ GHFollowers/GFTextField.swift
@@ -8,33 +8,33 @@
 import UIKit
 
 class GFTextField: UITextField {
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configure()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  
-  private func configure() {
-    translatesAutoresizingMaskIntoConstraints = false
     
-    layer.cornerRadius          = 10
-    layer.borderWidth           = 2
-    layer.borderColor           = UIColor.systemGray4.cgColor
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
     
-    textColor                   = .label
-    tintColor                   = .label
-    textAlignment               = .center
-    font                        = UIFont.preferredFont(forTextStyle: .title2)
-    adjustsFontSizeToFitWidth   = true
-    minimumFontSize             = 12
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    backgroundColor             = .tertiarySystemBackground
-    autocorrectionType          = .no
-    placeholder                 = "Enter a username"
-  }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        placeholder                 = "Enter a username"
+    }
 }
