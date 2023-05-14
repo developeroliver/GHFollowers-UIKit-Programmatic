0105eb6ffbd07d51011cbb038d061bdfed3ec49d
olive <developpeurolive.ios@gmail.com>
2023-05-14 15:31:52 +0200

    Create NetworkManager
    Create Network call

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -10,13 +10,28 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
-
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+                
+            case.success(let followers):
+                print(followers)
+                
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
-
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
 }
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 14/05/2023.
+//
+
+import Foundation
+
+enum Constants: String {
+    case textLogo = "bnb"
+}
